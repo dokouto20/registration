b01c58308b9832c8b5fe254accfe88290c64f93a
tomas <dokouto20@sps-prosek.ct>
2022-04-27 22:11:39 +0200

    done...........

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from turtle import delay, width
 from functools import partial
 import os
 
-
+# create table 
 #c.executescript("""
 #REATE TABLE people(
 #    id INTEGER PRIMARY KEY,
@@ -60,7 +60,7 @@ def check_lasname(vallastname,err):
     else:
         err[1] = 0
 
-#check if email is legit 
+# check if email is legit 
 def check_email(valemail,err):
     pattern = '^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
 
@@ -69,7 +69,7 @@ def check_email(valemail,err):
     else:
         err[2] = 0
 
-#check if num is real
+# check if num is real
 def check_phone(valphone,err):
     status = 0
     lenght = len(valphone)
@@ -88,12 +88,11 @@ def check_phone(valphone,err):
     else:
         err[3] = 0
 
-#check personal id 
+# check personal id 
 def check_persid(valpersid,err):
     #split and chcek brirth date
-    if valpersid != "OK" or valpersid != "ERROR":
         lenght = len(valpersid)
-        #pers id has 10 num and /
+        # pers id has 10 num and /
         if valpersid and lenght == 11:
             p = 0
             for i in valpersid:
@@ -102,21 +101,22 @@ def check_persid(valpersid,err):
                 else:
                     p = 1
             if p == 0:
-                #split pers id 
+                # split pers id 
                 splited = valpersid.split("/")
+                valpersid = splited[0]+splited[1]
                 sp1val = splited[0]
+                sp2val = splited[1]
                 n = 2
                 sp1 = [sp1val[i:i + n] for i in range(0, len(sp1val), n)]
-                print(sp1)
                 # isolate year month and day 
                 year = "20"+sp1[0]
                 month = sp1[1]
-                #womens has +50 on their birth month
+                # women has +50 on months of birth
                 if int(month) > 12:
                     month = int(month) - 50
                 day = sp1[2]
                 current_year = date.today().year
-                #check if date can exist 
+                # check if date can exist 
                 if year and month and day:
                     if int(year) > current_year:
                         err[4] = 0
@@ -127,20 +127,28 @@ def check_persid(valpersid,err):
                             if int(day) > 31:
                                 err[4] = 0
                             else:
-                                err[4] = 1
+                                # check last 4 digits 
+                                temp = valpersid[1::2]
+                                temp = [int(i) for i in temp]
+                                temp2 = valpersid[::2]
+                                temp2 = [int(i) for i in temp2]
+                                res = int(sum(temp)) - int(sum(temp2))
+                                lastnumber = int(valpersid[-1])
+                                if (lastnumber == 0 and res % 10 != 0) or (lastnumber != 0 and res % 11 != 0):
+                                    err[4] = 0
+                                else:
+                                    err[4] = 1
                 else:
                     err[4] = 0
         else:
             err[4] = 0
-    else:
-        err[4] = 0
 
-#get vals form entry and check it 
+# get values from entry and check it
 def getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, personal_id_entry, editstatus, id):
     # err list 
     err = [0,0,0,0,0]
 
-    #reset errors
+    # reset errors
     name_entry.config(highlightbackground = "white", highlightcolor= "white")
     lastname_entry.config(highlightbackground = "white", highlightcolor= "white")
     email_entry.config(highlightbackground = "white", highlightcolor= "white")
@@ -162,7 +170,7 @@ def getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, per
     num = 0
     status = 0
 
-    #delete text areas if everthing is ok 
+    # delete text areas if everthing is ok 
     for i in err:
         print(i)
         num += 1
@@ -181,6 +189,7 @@ def getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, per
             status += 1
 
     if status == 5:
+        # check if insert or update data 
         if editstatus == 0:
             # inset into database 
             connection = sqlite3.connect('database.db')
@@ -194,13 +203,15 @@ def getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, per
             connection.commit()
             connection.close()
 
-            #alert window 
+            # alert window 
             alert = Toplevel(tk)
             alert.geometry("295x220")
             alert.title("alert")
-            Label(alert, text='regisrace uspesna', font='ar 15 bold').grid(row=0, column=3)
-            Button(alert, text="přejit na prehled", command= lambda: alertfnc(alert)).grid(row=3, column=3)
+            alert.resizable(False, False)
+            Label(alert, text='Well done', font='ar 15 bold').place(x=100,y=50)
+            Button(alert, text="Go to overview", command= lambda: alertfnc(alert)).place(x=105,y=100)
         else:
+            # update
             top.destroy()
             connection = sqlite3.connect('database.db')
             c = connection.cursor()
@@ -218,8 +229,10 @@ def getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, per
             connection.close()
             alertfnc(alert=edit)
 
-#redirect to prehled 
+# redirect to overview 
 def alertfnc(alert):
+    global top 
+    top = Tk()
     top.destroy()
     alert.after(1000,lambda:alert.destroy())
     overview()
@@ -227,6 +240,7 @@ def alertfnc(alert):
 # print frame
 def print_frame(top):
 
+    # create mainframe 
     global mainframe
     mainframe = Frame(top)
     mainframe.pack(fill=BOTH, expand=1)
@@ -243,6 +257,7 @@ def print_frame(top):
     secondframe = Frame(mycanvas)
     mycanvas.create_window((0,0), window=secondframe, anchor="nw")
 
+    # db connect 
     connection = sqlite3.connect('database.db')
     c = connection.cursor()
     c.execute("SELECT * FROM people")
@@ -253,19 +268,20 @@ def print_frame(top):
     delY = 60
     ediY = 20
 
+    # print information
     for i in lenght:
 
         Textarea = Text(secondframe, width=70, height=5,)
         Textarea.grid(row=j,column=0,padx=10,pady=10)
-        Textarea.insert(END, "\n" +"name: "+result[i][1]+" lastname: "+result[i][2])
-        Textarea.insert(END, "\n" +"email: "+result[i][3]+" phone num: "+result[i][4])
-        Textarea.insert(END, "\n" +"personal id: "+result[i][5])
+        Textarea.insert(END, "\n" +"Name: "+result[i][1]+" Lastname: "+result[i][2])
+        Textarea.insert(END, "\n" +"Email: "+result[i][3]+" Phone number: "+result[i][4])
+        Textarea.insert(END, "\n" +"Personal id: "+result[i][5])
         Textarea.configure(state='disabled')
 
         functionedit = partial(editfnc, id=result[i][0])
-        edit = Button(secondframe, text="  edit  ", command=functionedit).place(x=520,y=ediY)
+        edit = Button(secondframe, text="  Edit  ", command=functionedit).place(x=520,y=ediY)
         functiondelete = partial(delete, id=result[i][0])
-        deletebutton = Button(secondframe, text="delete", command=functiondelete)
+        deletebutton = Button(secondframe, text="Delete", command=functiondelete)
         deletebutton.place(x=520,y=delY)
 
         delY += 104
@@ -275,7 +291,7 @@ def print_frame(top):
     connection.commit()
     connection.close()
 
-#delete from database
+# alertdelete registration from database
 def delete(id):
     valid = id 
     print(valid)
@@ -287,34 +303,35 @@ def delete(id):
     mainframe.destroy()
     print_frame(top)
 
-#prehled 
+# overview
 def overview():
-    global top
+    global top 
     top = Tk()
     top.geometry("610x600")
-    top.title("prehled")
+    top.title("Overview")
     top.resizable(False, False)
 
+    #print scrollable frame 
     print_frame(top)
 
     top.mainloop()
 
-#edit
+# edit
 def editfnc(id):
     global edit
     edit = Tk()
     edit.geometry("295x220")
-    edit.title("edit")
+    edit.title("Edit")
     edit.resizable(False, False)
 
-    Label(edit, text='edit', font='ar 15 bold').grid(row=0, column=3)
+    Label(edit, text='Edit', font='ar 15 bold').grid(row=0, column=3)
 
-    # colums
-    name = Label(edit, text='name')
-    lastname = Label(edit, text='lastname')
-    email = Label(edit, text='email')
-    phone_number = Label(edit, text='phone number')
-    presonal_id = Label(edit, text='personal id')
+    # entry
+    name = Label(edit, text='Name')
+    lastname = Label(edit, text='Lastname')
+    email = Label(edit, text='Email')
+    phone_number = Label(edit, text='Phone number')
+    presonal_id = Label(edit, text='Personal id')
 
     name.grid(row=1 ,column=2)
     lastname.grid(row=2 ,column=2)
@@ -328,6 +345,7 @@ def editfnc(id):
     phone_number_value = StringVar
     personal_id_value = StringVar
 
+    # select specific information by id 
     connection = sqlite3.connect('database.db')
     c = connection.cursor()
     c.execute("SELECT * FROM people WHERE id ="+str(id)+"")
@@ -358,22 +376,22 @@ def editfnc(id):
     
     edit.mainloop()
 
-# main win
+# main window
 def main(tk):
     # registration window 
     tk.geometry("295x220")
     tk.resizable(False, False)
-    tk.title("reg")
+    tk.title("Registration")
 
 
-    Label(tk, text='registration', font='ar 15 bold').grid(row=0, column=3)
+    Label(tk, text='Registration', font='ar 15 bold').grid(row=0, column=3)
 
-    # colums
-    name = Label(tk, text='name')
-    lastname = Label(tk, text='lastname')
-    email = Label(tk, text='email')
-    phone_number = Label(tk, text='phone number')
-    presonal_id = Label(tk, text='personal id')
+    # entry
+    name = Label(tk, text='Name')
+    lastname = Label(tk, text='Lastname')
+    email = Label(tk, text='Email')
+    phone_number = Label(tk, text='Phone number')
+    presonal_id = Label(tk, text='Personal id')
 
     name.grid(row=1 ,column=2)
     lastname.grid(row=2 ,column=2)
@@ -400,13 +418,15 @@ def main(tk):
     personal_id_entry.grid(row=5, column=3)
 
     # buttons 
-    Button(tk, text="submit", command= lambda:getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, personal_id_entry, editstatus=0, id = 0)).grid(row=7, column=3)
-    Button(tk, text="preheled", command= lambda:overview()).grid(row=8, column=3)
+    Button(tk, text="Submit", command= lambda:getvals(tk, name_entry, lastname_entry, email_entry, phone_number_entry, personal_id_entry, editstatus=0, id = 0)).grid(row=7, column=3)
+    Button(tk, text="Overview", command= lambda:overview()).grid(row=8, column=3)
 
 
     tk.mainloop()
 
 
+#create main 
 tk = Tk()
 main(tk)
+
 
